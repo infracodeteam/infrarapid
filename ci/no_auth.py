d0ffa2352c139df23157f4cf30bae60000b8d9f4
Sagi Shnaidman <einarum@gmail.com>
2021-01-25 00:17:44 +0200

    Find recent images automatically
    Find recent AMI in AWS according to users input.

--- ci/no_auth.py
+++ ci/no_auth.py
@@ -6,8 +6,6 @@ AWS = """
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
   s3_force_path_style         = true
-  access_key                  = "mock_access_key"
-  secret_key                  = "mock_secret_key"
 """
 AZURE = """
     skip_credentials_validation = true
