e968930c0f6940f736cc9467cc83a21093e1a266
Siddharth Gangadhar <lordlabakdas@protonmail.com>
2021-03-09 14:02:29 -0500

    pip package additions (#28)
    PIP packaging added
    
    Co-authored-by: lordlabakdas <siddharth@infrastructurecode.io>

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+setuptools.setup(
+    name="InfraRapid",
+    version="0.0.1",
+    author="InfraCode",
+    author_email="sergey@infrastructurecode.io",
+    description="InfraRapid package",
+    long_description="""Infrarapid tools allows you to create,
+                        configure and deploy same infrastructure
+                        over multiple cloud providers""",
+    long_description_content_type="text/markdown",
+    url="https://github.com/infracodeteam/infrarapid",
+    project_urls={
+        "Bug Tracker": "https://github.com/infracodeteam/infrarapid/issues/",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
